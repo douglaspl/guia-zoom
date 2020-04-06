958f4968b4fc799024c7e5f1ff52449d256c09c3
Vince <vicente.sarmento@coreskills.com.br>
2020-04-06 15:14:42 -0300

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+(function($){
+    // console.log(navigator.userAgent);
+    /* Adjustments for Safari on Mac */
+    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Mac') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+        // console.log('Safari on Mac detected, applying class...');
+        $('html').addClass('safari-mac'); // provide a class for the safari-mac specific css to filter with
+    }
+})(jQuery);
